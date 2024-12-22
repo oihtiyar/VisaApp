a3acbbaa39b9b5ccf59b259dbaa1149bdf445666
oihtiyar <50960588+oihtiyar@users.noreply.github.com>
2024-12-22 19:19:55 +0300

    Add files via upload

---
+++ VisaAppTests/VisaAppTests.swift
@@ -0,0 +1,16 @@
+//
+//  VisaAppTests.swift
+//  VisaAppTests
+//
+//  Created by oihtiyar on 16.12.2024.
+//
+
+import Testing
+
+struct VisaAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
